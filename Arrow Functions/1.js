8ab4730fc79dd07af4e5dff8051a9322a1670c8b
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-07-26 21:24:39 +0600

    Arrow functions

---
+++ Arrow Functions/1.js
@@ -0,0 +1,15 @@
+//traditional function
+
+function traditionalFunction (nums) {
+    return nums
+}
+console.log(traditionalFunction(10));
+
+//Arrow function
+
+//Step-1 to declare arrow function
+const numbers = (nums) => {
+    return nums;
+}
+
+console.log(40)
