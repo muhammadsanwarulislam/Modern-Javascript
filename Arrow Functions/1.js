c6098bb1c8d6bc6ee66a9a476207cbbb018884cb
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-07-26 22:05:03 +0600

    Arrow functions

--- Arrow Functions/1.js
+++ Arrow Functions/1.js
@@ -3,13 +3,16 @@
 function traditionalFunction (nums) {
     return nums
 }
-console.log(traditionalFunction(10));
+console.log(traditionalFunction(10))
 
 //Arrow function
 
 //Step-1 to declare arrow function
 const numbers = (nums) => {
-    return nums;
+    return nums
 }
+console.log(numbers(50))
 
-console.log(40)
+//Step-2 to declare arrow function, if it return only single statement
+const sortform = (nums) => nums
+console.log(sortform(10))
