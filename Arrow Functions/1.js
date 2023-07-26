54f8c9bed7316c91fc4097bced28769a0b4a7c1a
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-07-26 22:51:55 +0600

    Arrow functions

--- Arrow Functions/1.js
+++ Arrow Functions/1.js
@@ -16,3 +16,14 @@ console.log(numbers(50))
 //Step-2 to declare arrow function, if it return only single statement
 const sortform = (nums) => nums
 console.log(sortform(10))
+
+let group = {
+    title: 'Science',
+    students: ['Pete','Jone','Alice'],
+    showList: function() {
+        this.students.forEach(function(el){
+            console.log(`${el} ${this.title} Student `)
+        })
+    } 
+}
+group.showList()
